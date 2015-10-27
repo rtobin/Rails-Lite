9b239cfc3a39f4ca0fb536e02d7462c02093943c
Scott <dikowitz@gmail.com>
2015-10-27 15:06:32 -0400

    Phase 4: Add Sessions.

--- lib/phase4/session.rb
+++ lib/phase4/session.rb
@@ -6,17 +6,23 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      rl_cookie = req.cookies.find { |x| x.name == "_rails_lite_app" }
+      @stuff = rl_cookie.nil? ? {} : JSON.parse(rl_cookie.value)
+
     end
 
     def [](key)
+      @stuff[key]
     end
 
     def []=(key, val)
+      @stuff[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      res.cookies << WEBrick::Cookie.new("_rails_lite_app", @stuff.to_json)
     end
   end
 end
